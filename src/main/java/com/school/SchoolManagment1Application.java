067823afed98f6216cc8c48de8486a83093f3f07
91700 <91700@DESKTOP-094SIGK>
2022-07-30 12:38:55 +0530

    first commit

---
+++ src/main/java/com/school/SchoolManagment1Application.java
@@ -0,0 +1,13 @@
+package com.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagment1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagment1Application.class, args);
+	}
+
+}
